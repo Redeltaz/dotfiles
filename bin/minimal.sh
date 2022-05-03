50a8baad74fd2357617e058d6b27f4c144b2f117
Lucas CAMPISTRON <lcampistron@localhost.localdomain>
2022-05-03 14:46:45 +0200

    adjust zsh script

--- bin/minimal.sh
+++ bin/minimal.sh
@@ -8,14 +8,12 @@ zsh_setup () {
     echo "Starting zsh installation"
     echo "-------------------------"
     echo y | sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    chsh -s /bin/zsh
-    zsh
+    #chsh -s /bin/zsh
 
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
     echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
     echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >> ~/.zshrc
     cp ~/dotfiles/zsh/p10k ~/.p10k.zsh 
-    source ~/.zshrc 
 }
 
 zsh_setup 
