b7df45cc4e8eac6c454e539592caf9ae6bf75c5a
Redeltaz <lucascampistron95@gmail.com>
2022-05-06 15:42:28 +0200

    fix nvim config

--- bin/dev.sh
+++ bin/dev.sh
@@ -49,11 +49,15 @@ nvim_setup () {
     wget -P $install_path https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.deb
     sudo apt install $install_path/nvim-linux64.deb
     rm $install_path/nvim-linux64.deb
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+    # Needed for treesitter
     sudo apt install -y \
         gcc \
         build-essential \
         aptitude \
-        libstdc++
+        libstdc++6
 }
 
 basic_setup
