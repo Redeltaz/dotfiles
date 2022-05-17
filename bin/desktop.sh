09f133afbee4874ca6fd9ee074f1989adc7ba570
lucas <lucascampistron95@gmail.com>
2022-05-17 09:08:13 +0200

    add desktop script

---
+++ bin/desktop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install only the most important for dev enrionnement (on remote machine for exemple)
+
+install_path=/tmp/dotfiles-install
+
+if [ "$USER" = "root" ]; then
+    path=/root 
+else
+    path=/home/$USER
+fi
