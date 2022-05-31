bdd6583c165164339ce45b1833b4f66f8c6c480e
Redeltaz <lucascampistron95@gmail.com>
2022-05-31 23:47:08 +0200

    finish polybar config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,5 +3,6 @@
 killall -q polybar
 
 echo "---" | tee -a /tmp/polybar.log
-polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+MONITOR="HDMI-0" polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+MONITOR="DP-3" polybar main 2>&1 | tee -a /tmp/polybar.log & disown
 echo "Bars launched..."
