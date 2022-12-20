8ea4e8487c6e4abe754f7e227c4cc49aafbbce45
Redeltaz <lucascampistron95@gmail.com>
2022-12-20 17:19:21 +0100

    wip: refactoring installation process

--- install.py
+++ install.py
@@ -1,3 +1,31 @@
 #!/usr/bin/env python
 
-print("Hello")
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("action", nargs="?", choices=["install", "uninstall", "update"],
+                    help="Chose what action to execute")
+parser.add_argument('--skip', "-s", nargs="*", default=False,
+                    help='If set, software choosed while not be affected by the action.')
+
+args = parser.parse_args()
+print(args)
+
+if not args.action:
+    print("You need to choose an action")
+    exit(1)
+
+softwares = [
+    "alacritty",
+    "dunst",
+    "firefox",
+    "git",
+    "i3",
+    "nvim",
+    "picom",
+    "polybar",
+    "rofi",
+    "tmux",
+    "zsh"
+]
