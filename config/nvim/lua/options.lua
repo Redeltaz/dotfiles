821e992ac63f165ef434c1d13488f01b4347517e
Redeltaz <campistron.lucas@gmail.com>
2024-02-29 09:53:21 +0100

    add nvim scrolloff

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -6,6 +6,7 @@ vim.opt.termguicolors = true --allow terminal colors
 vim.opt.tabstop = 4 --max width of a tab character
 vim.opt.shiftwidth = 4 --size of and indent
 vim.opt.expandtab = true --tab key insert spaces instead of tab
+vim.opt.scrolloff = 999
 
 -- Setup nvim to CWD when path specified
 if next(vim.fn.argv()) ~= nil then
